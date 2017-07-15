4ef1e11255f00da68e00340f586f66f253e38954
cai wei <453717310@qq.com>
2017-07-15 17:43:52 +0800

    test类提交

---
+++ src/cn/bdqn/action/test.java
@@ -0,0 +1,18 @@
+package cn.bdqn.action;
+
+
+/**
+ * Created by dll on 2017/7/15.
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("11111");
+        System.out.println("11111");
+        System.out.println("11111");
+        System.out.println("11111");
+        System.out.println("11111");
+        System.out.println("11111");
+        System.out.println("11111");
+
+    }
+}
