57232b0b9a057165286c0d0e879854e56867dfbe
shaunlix <shaunlix@163.com>
2017-07-15 18:18:38 +0800

    冲突一下

--- src/cn/bdqn/action/test.java
+++ src/cn/bdqn/action/test.java
@@ -6,13 +6,13 @@ package cn.bdqn.action;
  */
 public class test {
     public static void main(String[] args) {
+        String str = "hello";
+        System.out.println("xyz");
         System.out.println("11111");
         System.out.println("11111");
+        str += " world";
         System.out.println("11111");
-        System.out.println("11111");
-        System.out.println("11111");
-        System.out.println("11111");
-        System.out.println("11111");
+        System.out.println("str:" + str);
 
     }
 }
