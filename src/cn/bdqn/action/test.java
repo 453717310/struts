0d9f51324ec14f41e8ffe761416e27ddbc16ba41
cai wei <453717310@qq.com>
2017-07-15 17:56:46 +0800

    分支合并提交

--- src/cn/bdqn/action/test.java
+++ src/cn/bdqn/action/test.java
@@ -8,11 +8,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("11111");
         System.out.println("11111");
-        System.out.println("11111");
-        System.out.println("11111");
-        System.out.println("11111");
-        System.out.println("11111");
-        System.out.println("11111");
+
 
     }
 }
